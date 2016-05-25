07884cbf150bd7ee3558b897b25ea4b21c0f2deb
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2016-05-25 17:22:35 +0200

    add ros control module to manager

--- thormang3_manager/src/thormang3_manager.cpp
+++ thormang3_manager/src/thormang3_manager.cpp
@@ -46,6 +46,7 @@
 #include "thormang3_head_control_module/head_control_module.h"
 #include "thormang3_manipulation_module/manipulation_module.h"
 #include "thormang3_walking_module/walking_module.h"
+#include "thormang3_ros_control_module/ros_control_module.h"
 
 
 using namespace thormang3;
@@ -100,6 +101,7 @@ int main(int argc, char **argv)
     controller->addMotionModule((robotis_framework::MotionModule*)ManipulationModule::getInstance());
     controller->addMotionModule((robotis_framework::MotionModule*)HeadControlModule::getInstance());
     controller->addMotionModule((robotis_framework::MotionModule*)WalkingMotionModule::getInstance());
+    controller->addMotionModule((robotis_framework::MotionModule*)RosControlModule::getInstance());
 
     controller->startTimer();
 
