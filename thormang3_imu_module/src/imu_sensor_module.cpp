01f7e572d1edef9db47f9b214a07c530c4307afe
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2016-06-03 19:13:06 +0200

    Implemented thormang3_imu_module. IMU data transforms from simulated and real sensors to be checked.
    Conflicts:
    	thormang3_manager/src/thormang3_manager.cpp

---
+++ thormang3_imu_module/src/imu_sensor_module.cpp
@@ -0,0 +1,124 @@
+#include <thormang3_imu_module/imu_sensor_module.h>
+
+#include <tf/tf.h>
+#include <robotis_controller_msgs/StatusMsg.h>
+
+
+
+using namespace thormang3;
+
+ImuSensor::ImuSensor()
+  : control_cycle_msec_(8)
+  , gazebo_robot_name_("robotis")
+  , gazebo_mode_(false)
+{
+  module_name_ = "thormang3_imu_sensor_module"; // set unique module name
+
+  result_["imu_orientation_q_x_raw"] = 0.0;
+  result_["imu_orientation_q_y_raw"] = 0.0;
+  result_["imu_orientation_q_z_raw"] = 0.0;
+  result_["imu_orientation_q_w_raw"] = 1.0;
+
+  result_["imu_orientation_r_raw"] = 0.0;
+  result_["imu_orientation_p_raw"] = 0.0;
+  result_["imu_orientation_y_raw"] = 0.0;
+
+  result_["imu_linear_acceleration_x_raw"] = 0.0;
+  result_["imu_linear_acceleration_y_raw"] = 0.0;
+  result_["imu_linear_acceleration_z_raw"] = 0.0;
+
+  result_["imu_angular_velocity_x_raw"] = 0.0;
+  result_["imu_angular_velocity_y_raw"] = 0.0;
+  result_["imu_angular_velocity_z_raw"] = 0.0;
+
+  result_["imu_orientation_q_x_filtered"] = 0.0;
+  result_["imu_orientation_q_y_filtered"] = 0.0;
+  result_["imu_orientation_q_z_filtered"] = 0.0;
+  result_["imu_orientation_q_w_filtered"] = 1.0;
+
+  result_["imu_orientation_r_filtered"] = 0.0;
+  result_["imu_orientation_p_filtered"] = 0.0;
+  result_["imu_orientation_y_filtered"] = 0.0;
+
+  result_["imu_linear_acceleration_x_filtered"] = 0.0;
+  result_["imu_linear_acceleration_y_filtered"] = 0.0;
+  result_["imu_linear_acceleration_z_filtered"] = 0.0;
+
+  result_["imu_angular_velocity_x_filtered"] = 0.0;
+  result_["imu_angular_velocity_y_filtered"] = 0.0;
+  result_["imu_angular_velocity_z_filtered"] = 0.0;
+}
+
+ImuSensor::~ImuSensor()
+{
+  queue_thread_.join();
+}
+
+void ImuSensor::initialize(const int control_cycle_msec, robotis_framework::Robot* robot)
+{
+  control_cycle_msec_ = control_cycle_msec;
+
+  queue_thread_       = boost::thread(boost::bind(&ImuSensor::msgQueueThread, this));
+}
+
+void ImuSensor::publishStatusMsg(unsigned int type, std::string msg)
+{
+  robotis_controller_msgs::StatusMsg _status;
+  _status.header.stamp = ros::Time::now();
+  _status.type = type;
+  _status.module_name = "IMU";
+  _status.status_msg = msg;
+
+  thormang3_imu_status_pub_.publish(_status);
+}
+
+void ImuSensor::imuSensorCallback(const sensor_msgs::Imu::ConstPtr msg, const std::string& tag)
+{
+  boost::mutex::scoped_lock lock(imu_sensor_mutex_);
+
+  result_["imu_orientation_q_x_" + tag] = msg->orientation.x;
+  result_["imu_orientation_q_y_" + tag] = msg->orientation.y;
+  result_["imu_orientation_q_z_" + tag] = msg->orientation.z;
+  result_["imu_orientation_q_w_" + tag] = msg->orientation.w;
+
+  tf::Quaternion q;
+  tf::quaternionMsgToTF(msg->orientation, q);
+  tf::Matrix3x3(q).getRPY(result_["imu_orientation_r_" + tag], result_["imu_orientation_p_" + tag], result_["imu_orientation_y_" + tag]);
+
+  result_["imu_linear_acceleration_x_" + tag] = msg->linear_acceleration.x;
+  result_["imu_linear_acceleration_y_" + tag] = msg->linear_acceleration.y;
+  result_["imu_linear_acceleration_z_" + tag] = msg->linear_acceleration.z;
+
+  result_["imu_angular_velocity_x_" + tag] = msg->angular_velocity.x;
+  result_["imu_angular_velocity_y_" + tag] = msg->angular_velocity.y;
+  result_["imu_angular_velocity_z_" + tag] = msg->angular_velocity.z;
+}
+
+void ImuSensor::msgQueueThread()
+{
+  ros::NodeHandle     _ros_node;
+  ros::CallbackQueue  _callback_queue;
+
+  _ros_node.setCallbackQueue(&_callback_queue);
+
+  /* subscriber */
+  //ros::Subscriber ft_calib_command_sub	= _ros_node.subscribe("robotis/wrist_ft/ft_calib_command",	1, &ThorMang3ImuSensor::FTSensorCalibrationCommandCallback, this);
+  ros::Subscriber imu_raw_sub = _ros_node.subscribe<sensor_msgs::Imu>(gazebo_mode_ ? "/gazebo/" + gazebo_robot_name_ + "/sensor/imu" : "sensor/imu/raw",	1,
+                                                    boost::bind(&ImuSensor::imuSensorCallback, this, _1, std::string("raw")));
+  ros::Subscriber imu_filtered_sub = _ros_node.subscribe<sensor_msgs::Imu>(gazebo_mode_ ? "/gazebo/" + gazebo_robot_name_ + "/sensor/imu" : "sensor/imu/filtered",	1,
+                                                    boost::bind(&ImuSensor::imuSensorCallback, this, _1, std::string("filtered")));
+
+  /* publisher */
+  thormang3_imu_status_pub_	= _ros_node.advertise<robotis_controller_msgs::StatusMsg>("robotis/status", 1);
+
+  while(_ros_node.ok())
+  {
+    _callback_queue.callAvailable();
+    usleep(100);
+  }
+}
+
+void ImuSensor::process(std::map<std::string, robotis_framework::Dynamixel*> /*dxls*/, std::map<std::string, robotis_framework::Sensor*> /*sensors*/)
+{
+  // nothing to do
+}
--- thormang3_manager/src/thormang3_manager.cpp
+++ thormang3_manager/src/thormang3_manager.cpp
@@ -38,6 +38,7 @@
 #include "robotis_controller/robotis_controller.h"
 
 /* Sensor Module Header */
+#include "thormang3_imu_module/imu_sensor_module.h"
 #include "thormang3_feet_ft_module/feet_force_torque_sensor_module.h"
 
 /* Motion Module Header */
@@ -65,14 +66,16 @@ int main(int argc, char **argv)
 
     std::string init_file   = nh.param<std::string>("init_file_path", "");
 
+    std::string gazebo_robot_name = nh.param<std::string>("gazebo_robot_name", controller->gazebo_robot_name_);
+    controller->gazebo_robot_name_ = gazebo_robot_name;
+    ImuSensor::getInstance()->gazebo_robot_name_ = gazebo_robot_name;
+
     /* gazebo simulation */
-    controller->gazebo_mode_ = nh.param<bool>("gazebo", false);
-    if(controller->gazebo_mode_ == true)
+    if(nh.param<bool>("gazebo", false))
     {
+        controller->gazebo_mode_ = true;
+        ImuSensor::getInstance()->gazebo_mode_ = true;
         ROS_WARN("SET TO GAZEBO MODE!");
-        std::string robot_name = nh.param<std::string>("gazebo_robot_name", "");
-        if(robot_name != "")
-            controller->gazebo_robot_name_ = robot_name;
     }
 
     if(robot_file == "")
@@ -88,11 +91,12 @@ int main(int argc, char **argv)
     }
 
     if(offset_file != "")
-        controller->loadOffset(offset_file);
+        controller->loadOffsets(offset_file);
 
     sleep(1);
 
     /* Add Sensor Module */
+    controller->addSensorModule((robotis_framework::SensorModule*)ImuSensor::getInstance());
     controller->addSensorModule((robotis_framework::SensorModule*)FeetForceTorqueSensor::getInstance());
 
     /* Add Motion Module */
