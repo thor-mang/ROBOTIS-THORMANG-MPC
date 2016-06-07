18a4ca7bd6b4ed1f5671def8b49b524483921070
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2016-06-07 22:05:59 +0200

    Controller manager is now updated although when the module itself has been deactivated.

--- thormang3_ros_control_module/include/thormang3_ros_control_module/ros_control_module.h
+++ thormang3_ros_control_module/include/thormang3_ros_control_module/ros_control_module.h
@@ -66,27 +66,32 @@ public:
   bool IsRunning() override;
 
 private:
+  void ControllerManagerThread();
   void QueueThread();
 
   int control_cycle_msec_;
+  boost::thread controller_manager_thread_;
   boost::thread queue_thread_;
   ros::Time last_time_stamp_;
-  
+  bool reset_controllers_;
+
+  boost::mutex ros_control_mutex_;
+
   /** ROS CONTROL PART */
   boost::shared_ptr<controller_manager::ControllerManager> controller_manager_;
-  
+
   // IMU
   hardware_interface::ImuSensorInterface imu_sensor_interface_;
-  hardware_interface::ImuSensorHandle::Data imu_data;
-  double imu_orientation[4];
-  double imu_angular_velocity[3];
-  double imu_linear_acceleration[3];
+  hardware_interface::ImuSensorHandle::Data imu_data_;
+  double imu_orientation_[4];
+  double imu_angular_velocity_[3];
+  double imu_linear_acceleration_[3];
 
   // FT-Sensors
   hardware_interface::ForceTorqueSensorInterface force_torque_sensor_interface_;
   std::map<std::string, double[3]> force_;
   std::map<std::string, double[3]> torque_;
-  
+
   // joint interfaces
   hardware_interface::JointStateInterface jnt_state_interface_;
   hardware_interface::PositionJointInterface jnt_pos_interface_;
