8597a5863e3e2e862e8ca7c301ab3a88db62e0f6
Jay-Song <hjsong@robotis.com>
2016-06-07 18:45:04 +0900

    modified ROS c++ coding style is applied

--- thormang3_kinematics_dynamics/include/thormang3_kinematics_dynamics/kinematics_dynamics.h
+++ thormang3_kinematics_dynamics/include/thormang3_kinematics_dynamics/kinematics_dynamics.h
@@ -52,21 +52,13 @@ enum TreeSelect {
   WholeBody
 };
 
-class ThorMang3KinematicsDynamics
+class KinematicsDynamics
 {
 
 public:
-
-  LinkData *thormang3_link_data [ ALL_JOINT_ID + 1 ];
-
-  double thigh_length_m;
-  double calf_length_m;
-  double ankle_length_m;
-  double leg_side_offset_m;
-
-  ThorMang3KinematicsDynamics();
-  ~ThorMang3KinematicsDynamics();
-  ThorMang3KinematicsDynamics(TreeSelect tree);
+  KinematicsDynamics();
+  ~KinematicsDynamics();
+  KinematicsDynamics(TreeSelect tree);
 
   std::vector<int> findRoute(int to);
   std::vector<int> findRoute(int from, int to);
@@ -86,11 +78,81 @@ public:
 
   // with weight
   bool calcInverseKinematics(int to, Eigen::MatrixXd tar_position, Eigen::MatrixXd tar_orientation, int max_iter, double ik_err , Eigen::MatrixXd weight);
-  bool calcInverseKinematics(int from, int to, Eigen::MatrixXd tar_position, Eigen::MatrixXd tar_orientation, int max_iter, double ik_err , Eigen::MatrixXd weight);
+  bool calcInverseKinematics(int from, int to, Eigen::MatrixXd tar_position,
+      Eigen::MatrixXd tar_orientation, int max_iter, double ik_err,
+      Eigen::MatrixXd weight) {
+    bool ik_success = false;
+    bool limit_success = false;
+    //  calcForwardKinematics(0);
+    std::vector<int> idx = findRoute(from, to);
+    /* weight */
+    Eigen::MatrixXd weight_matrix = Eigen::MatrixXd::Identity(idx.size(),
+        idx.size());
+    for (int ix = 0; ix < idx.size(); ix++)
+      weight_matrix.coeffRef(ix, ix) = weight.coeff(idx[ix], 0);
+    /* damping */
+    Eigen::MatrixXd eval = Eigen::MatrixXd::Zero(6, 6);
+    double p_damping = 1e-5;
+    double R_damping = 1e-5;
+    for (int ix = 0; ix < 3; ix++) {
+      eval.coeffRef(ix, ix) = p_damping;
+      eval.coeffRef(ix + 3, ix + 3) = R_damping;
+    }
+    /* ik */
+    for (int iter = 0; iter < max_iter; iter++) {
+      Eigen::MatrixXd jacobian = calcJacobian(idx);
+      Eigen::MatrixXd curr_position = thormang3_link_data_[to]->position;
+      Eigen::MatrixXd curr_orientation = thormang3_link_data_[to]->orientation;
+      Eigen::MatrixXd err = calcVWerr(tar_position, curr_position,
+          tar_orientation, curr_orientation);
+      if (err.norm() < ik_err) {
+        ik_success = true;
+        break;
+      } else
+        ik_success = false;
+
+      Eigen::MatrixXd jacobian_trans = (jacobian * weight_matrix
+          * jacobian.transpose() + eval);
+      Eigen::MatrixXd jacobian_inv = weight_matrix * jacobian.transpose()
+          * jacobian_trans.inverse();
+      Eigen::MatrixXd delta_angle = jacobian_inv * err;
+      for (int id = 0; id < idx.size(); id++) {
+        int joint_id = idx[id];
+        thormang3_link_data_[joint_id]->joint_angle += delta_angle.coeff(id);
+      }
+      calcForwardKinematics(0);
+    }
+    /* check joint limit */
+    for (int id = 0; id < idx.size(); id++) {
+      int _joint_num = idx[id];
+      if (thormang3_link_data_[_joint_num]->joint_angle
+          >= thormang3_link_data_[_joint_num]->joint_limit_max) {
+        limit_success = false;
+        break;
+      } else if (thormang3_link_data_[_joint_num]->joint_angle
+          <= thormang3_link_data_[_joint_num]->joint_limit_min) {
+        limit_success = false;
+        break;
+      } else
+        limit_success = true;
+    }
+    if (ik_success == true && limit_success == true)
+      return true;
+    else
+      return false;
+  }
 
   bool calcInverseKinematicsForLeg(double *out, double x, double y, double z, double roll, double pitch, double yaw);
   bool calcInverseKinematicsForRightLeg(double *out, double x, double y, double z, double roll, double pitch, double yaw);
   bool calcInverseKinematicsForLeftLeg(double *out, double x, double y, double z, double roll, double pitch, double yaw);
+
+
+  LinkData *thormang3_link_data_ [ ALL_JOINT_ID + 1 ];
+
+  double thigh_length_m_;
+  double calf_length_m_;
+  double ankle_length_m_;
+  double leg_side_offset_m_;
 };
 
 }
